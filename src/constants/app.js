4a1634797e3d5bc5719dbc5593bcd8d1db2e87ab
shridhar.kandikatla <skandikatla.extern@europ-assistance.in>
2026-08-13 10:22:41 +0530

    feat: rebrand app to Eportal

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,3 +1,3 @@
-export const APP_NAME = 'EduCRM';
+export const APP_NAME = 'Eportal';
 export const APP_INITIAL = APP_NAME.charAt(0).toUpperCase() || 'E';
-export const APP_UNIVERSITY_NAME = 'EduCRM University';
+export const APP_UNIVERSITY_NAME = 'Eportal';
